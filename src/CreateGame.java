7e67b139571f5c56b39b57e41f4797a9849ec50c
mtam2 <maximtea.cloud@gmail.com>
2015-04-26 22:09:56 -0400

    Monsters never defend first!
    Bugged typeB action sequence for monsters in createGame causes monsters
    to forever repeat the same turn without any change.

--- src/CreateGame.java
+++ src/CreateGame.java
@@ -61,8 +61,8 @@ public class CreateGame
 		
 		//Monster objects are created and added to tempMonsterArray data structure
 		tempMonsterArray[0] = new Monster(0,"Sea Tortoise", "an angry spiky-shelled tortoise.", 50, 5, 0.9, typeA );		
-		tempMonsterArray[1] = new Monster(1,"Man-Shark", "a chimera with a human body and a shark head.", 55, 10, 0.3, typeB );		
-		tempMonsterArray[2] = new Monster(2,"Jaws", "the famous 25-foot shark with an appalling appetite for human flesh.", 55, 10, 0.1, typeB );		
+		tempMonsterArray[1] = new Monster(1,"Man-Shark", "a chimera with a human body and a shark head.", 55, 10, 0.3, typeA );		
+		tempMonsterArray[2] = new Monster(2,"Jaws", "the famous 25-foot shark with an appalling appetite for human flesh.", 55, 10, 0.1, typeA );		
 		tempMonsterArray[3] = new Monster(3,"Zoidberg", "a lobster who is a poor excuse for a human doctor.", 60, 5, 0, typeA );		
 		tempMonsterArray[4] = new Monster(4,"Giant Jellyfish", "the largest kind of cnidarian in the world with hundreds of tentacles.", 50, 10, 0.5, typeA );		
 		tempMonsterArray[5] = new Monster(5,"Sea Serpent", "an amphibious dragon with vicious fangs.", 60, 10, 0.6, typeA );
@@ -71,7 +71,7 @@ public class CreateGame
 		tempMonsterArray[8] = new Monster(8,"Siren", "a seducing winged creature with a scream that could be heard from miles away.", 50, 10, 0.5, typeA );	
 		tempMonsterArray[9] = new Monster(9,"Hydra", "a multi-headed sea snake with regenerative abilties.", 55, 5, 1, typeA );
 		tempMonsterArray[10] = new Monster(10,"Medusa", "a famed sea serpent with powers to freeze you for a mere glance at her eyes.", 70, 10, 0.2, typeA );
-		tempMonsterArray[11] = new Monster(11,"Giant Squid", "a squid with tentacles amassing up to 44 feet.", 50, 5, 0.3, typeB );
+		tempMonsterArray[11] = new Monster(11,"Giant Squid", "a squid with tentacles amassing up to 44 feet.", 50, 5, 0.3, typeA );
 		tempMonsterArray[12] = new Monster(12,"Sea Bear", "an enormous fish with a bear head.", 65, 10, 0.5, typeA );	
 		tempMonsterArray[13] = new Monster(13,"Kraken", "the mysterious, merciless giant squid hungry for human.", 80, 15, 0.9, typeA );
 		tempMonsterArray[14] = new Monster(14,"Cthulhu", "the renowned sea god bearing the ability to control all sea creatures.", 100, 15, 1, typeC );	
