a7bd59d319d55f63ea9f1c04ca09c3d056b52d7c
mtam2 <maximtea.cloud@gmail.com>
2015-04-26 21:10:30 -0400

    Monster descriptions and lowered health overall
    I do not know how to implement all 15 monsters into the game with your
    random generation methods.

--- src/CreateGame.java
+++ src/CreateGame.java
@@ -22,7 +22,7 @@ public class CreateGame
 {
 	
 	public static Room[] tempRoomArray = new Room[30];
-	public static Monster[] tempMonsterArray = new Monster[10];
+	public static Monster[] tempMonsterArray = new Monster[15];
 	public static Puzzle[] tempPuzzleArray = new Puzzle[10];
 	public static ArrayList<String> descriptionList = new ArrayList<String>(30);
 	
@@ -55,23 +55,27 @@ public class CreateGame
 	{
 		
 		//*** Basic Action Sequence Array used during Monster Construction ***
-		tempActionSequence[0] = Action.attack;
-		tempActionSequence[1] = Action.defend;
-		tempActionSequence[2] = Action.attack;
-		tempActionSequence[3] = Action.defend;
+		Action[] typeA = {Action.attack,Action.defend,Action.attack,Action.defend};
+		Action[] typeB = {Action.defend,Action.attack,Action.defend,Action.attack};
+		Action[] typeC = {Action.attack,Action.attack,Action.attack,Action.attack};		
 		
 		//Monster objects are created and added to tempMonsterArray data structure
-		tempMonsterArray[0] = new Monster(0, "Monster 1", "Desc. 1", 100, 10,0.5, tempActionSequence);
-		tempMonsterArray[1] = new Monster(1, "Monster 2", "Desc. 2", 100, 10,0.5, tempActionSequence);
-		tempMonsterArray[2] = new Monster(2, "Monster 3", "Desc. 3", 100, 10,0.5, tempActionSequence);
-		tempMonsterArray[3] = new Monster(3, "Monster 4", "Desc. 4", 100, 10,0.5, tempActionSequence);
-		tempMonsterArray[4] = new Monster(4, "Monster 5", "Desc. 5", 100, 10,0.5, tempActionSequence);
-		tempMonsterArray[5] = new Monster(5, "Monster 6", "Desc. 6", 100, 10,0.5, tempActionSequence);
-		tempMonsterArray[6] = new Monster(6, "Monster 7", "Desc. 7", 100, 10,0.5, tempActionSequence);
-		tempMonsterArray[7] = new Monster(7, "Monster 8", "Desc. 8", 100, 10,0.5, tempActionSequence);
-		tempMonsterArray[8] = new Monster(8, "Monster 9", "Desc. 9", 100, 10,0.5, tempActionSequence);
-		tempMonsterArray[9] = new Monster(9, "Monster 10", "Desc. 10", 100, 10,0.5, tempActionSequence);
-	
+		tempMonsterArray[0] = new Monster(0,"Sea Tortoise", "an angry spiky-shelled tortoise.", 50, 5, 0.9, typeA );		
+		tempMonsterArray[1] = new Monster(1,"Man-Shark", "a chimera with a human body and a shark head.", 55, 10, 0.3, typeB );		
+		tempMonsterArray[2] = new Monster(2,"Jaws", "the famous 25-foot shark with an appalling appetite for human flesh.", 55, 10, 0.1, typeB );		
+		tempMonsterArray[3] = new Monster(3,"Zoidberg", "a lobster who is a poor excuse for a human doctor.", 60, 5, 0, typeA );		
+		tempMonsterArray[4] = new Monster(4,"Giant Jellyfish", "the largest kind of cnidarian in the world with hundreds of tentacles.", 50, 10, 0.5, typeA );		
+		tempMonsterArray[5] = new Monster(5,"Sea Serpent", "an amphibious dragon with vicious fangs.", 60, 10, 0.6, typeA );
+		tempMonsterArray[6] = new Monster(6,"Spider-Crab", "staring at you intently with its eight eyes.", 60, 10, 0.7, typeC );
+		tempMonsterArray[7] = new Monster(7,"Mermaid", "a fabled creature with a torso of a human and a tail of a fish.",50, 5, 0.2, typeA );
+		tempMonsterArray[8] = new Monster(8,"Siren", "a seducing winged creature with a scream that could be heard from miles away.", 50, 10, 0.5, typeA );	
+		tempMonsterArray[9] = new Monster(9,"Hydra", "a multi-headed sea snake with regenerative abilties.", 55, 5, 1, typeA );
+		tempMonsterArray[10] = new Monster(10,"Medusa", "a famed sea serpent with powers to freeze you for a mere glance at her eyes.", 70, 10, 0.2, typeA );
+		tempMonsterArray[11] = new Monster(11,"Giant Squid", "a squid with tentacles amassing up to 44 feet.", 50, 5, 0.3, typeB );
+		tempMonsterArray[12] = new Monster(12,"Sea Bear", "an enormous fish with a bear head.", 65, 10, 0.5, typeA );	
+		tempMonsterArray[13] = new Monster(13,"Kraken", "the mysterious, merciless giant squid hungry for human.", 80, 15, 0.9, typeA );
+		tempMonsterArray[14] = new Monster(14,"Cthulhu", "the renowned sea god bearing the ability to control all sea creatures.", 100, 15, 1, typeC );	
+
 	}
 	
 	/**
